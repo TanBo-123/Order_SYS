1fe6684d8c881e45a07a27c7c9fd5fa7c783d6f4
Tan Bo <Bo.X.Tan@sony.com>
2019-08-07 19:41:54 +0800

    add order ai

--- Ordersys_AI.py
+++ Ordersys_AI.py
@@ -14,16 +14,22 @@ random_num = 80
 
 def Export_menu(menu,score,num):
     menu_len = len(menu)-1
-    menu_list = []
+    menu_list = []  #[菜品，评分，概率，选分]
     random_list = np.random.randint(0,menu_len,menu_len*random_num)
     for menu_cell,score_cell in zip(menu[1:],score[1:]):
-        menu_list.append([menu_cell.value,score_cell.value,0])
-    print(menu_list)
+        menu_list.append([menu_cell.value,score_cell.value,0,0])
+    #print(menu_list)
     for i in range(len(random_list)):
         menu_list[random_list[i]][2] += 1
-    print(menu_list)
-    print(menu_list[np.lexsort(menu_list.T)])
-    pass
+    for i in range(menu_len):
+        menu_list[i][3] = menu_list[i][1]*menu_list[i][2]/100
+    #print(menu_list)
+    menu_temp = menu_list.copy()
+    #print(menu_temp)
+    menu_list.sort(key=lambda x:x[3],reverse=True)
+    print(menu_list[0:num])
+    #print(menu_temp,'\n'+'\n',menu_list)
+    return [menu_temp.index(menu_list[i]) for i in range(num)]
 
 
 
@@ -35,6 +41,6 @@ print(len(ws["A"]), type(ws["A"]))
 #for cell in ws["A"]:
 #    print(type(cell),cell.value)
 
-Export_menu(ws["A"],ws["C"],3)
-
+L = Export_menu(ws["A"],ws["C"],3)
+print("L = ",L)
 wb.save(fpath)
